c81624f55e2ab090585333db80a610f68bed671e
Anna Marie Cooper and Kaila Goff <kaila@>
2018-02-08 16:06:56 -0800

    third spec passing

--- spec/prime_numbers_spec.rb
+++ spec/prime_numbers_spec.rb
@@ -11,4 +11,8 @@ describe ('#sifting') do
     prime_return = Prime.new()
     expect(prime_return.sifting(7)).to(eq([2, 3, 5, 7]))
   end
+  it ("return all of the prime numbers less than the input number") do
+    prime_return = Prime.new()
+    expect(prime_return.sifting(13)).to(eq([2, 3, 5, 7, 11, 13]))
+  end
 end
